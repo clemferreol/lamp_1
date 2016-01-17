0e31fab71b5bda2a69a96a2a7af32e7cb3c8935e
clemferreol <cleol@9online.fr>
2016-01-17 20:12:49 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,52 @@
+<?php 
+require_once("config/dbconf.php");
+session_start();
+if(isset($_POST['logout'])){
+  unset($_SESSION['user']);
+}
+if(isset($_SESSION['user'])){
+    header("Location: /");
+    exit;
+}
+$errormessage = null;
+if(isset($_POST['username'])){
+  global $config;
+  $pdo = new PDO($config['host'], $config['user'], $config['password']);
+  $stmt = $pdo->prepare("SELECT * FROM users 
+                          WHERE login = :login"
+                        );
+    
+  $stmt->bindParam("login",$_POST['username']);
+  $stmt->execute();
+  $result = $stmt->fetch();
+  if($result === false){
+    $errormessage = "Wrong username";
+  }elseif (empty($_POST["password"])) {
+    $errormessage = "No password";
+  }elseif (sha1($_POST["password"]) != $result["password"]) {
+    $errormessage = "Wrong password";
+  }else{
+    $_SESSION['user'] = $result["login"];
+    $_SESSION['best_score'] = $result['best_score'];
+    header("Location: /");
+    exit;
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  Merci de vous connecter :
+  <form method="POST">
+  Login : <input type="text" name="username"><br>
+  Password : <input type="password" name="password"><br>
+  <input type="submit" value="Log in">
+  </form>
+  <?php echo $errormessage;?>
+</body>
+</html>
